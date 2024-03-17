0d1ff654d15e7e5fd0b5a3738b948cbec2e33391
achakour1337 <achakour@student.1337.ma>
2024-03-17 14:51:49 +0000

    choffoni

---
+++ ft_check_map.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_map.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achakour <achakour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 11:12:51 by achakour          #+#    #+#             */
+/*   Updated: 2024/03/17 12:15:38 by achakour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+char    *get_map(char *file_name)
+{
+    char    **buff;
+    char    *tmp;
+    int     len;
+    int     fd;
+    int     i;
+
+    i = 0;
+    len = 0;
+    fd = open(file_name, O_WRONLY);
+    tmp = get_next_line(fd);
+    if (!tmp)
+        return (NULL);
+    while (tmp)
+    {
+        tmp = get_next_line(fd);
+        free (tmp);
+        ++len;
+        ++i;
+    }
+    if (len > 0)
+        buff = (char *)malloc(sizeof(char) * len + 1);
+    if (!buff)
+        return (NULL);
+    ft_fill_buff(buff, len);
+    return (buff);
+}
+
+int check_coins(char **map)
+{
+    int coins;
+    int i;
+    int j;
+
+    i = 0;
+    coins = 0;
+    while (map[i])
+    {
+        j = 0;
+        while (map[i][j])
+        {
+            if (map[i][j] == 'C')
+            {
+                return (0);
+            }
+            ++j;
+        }
+        ++i;
+    }
+    return (1);
+}
+
+int check_exit_player(char **map)
+{
+    int player;
+    int exit;
+    int i;
+    int j;
+
+    i = -1;
+    exit = 0;
+    player = 0;
+    while (map[++i])
+    {
+        j = -1;
+        while (map[i][j])
+        {
+            if (map[i][j] == 'P')
+            {
+                player += 1;
+            }
+            else if (map[i][j] == 'E')
+                exit += 1;
+            ++j;
+        }
+    }
+    if (player > 1 || exit > 1)
+        return (0);
+    return (1);
+}
+
+int is_valid_map(char **map, int row, int sides)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (map[i])
+    {
+        j = 0;
+        while (map[i][j])
+        {
+            if ((i == 0 || i == row) && map[i][j] != '1')
+            {
+                return (0);
+            }
+            else if ((j == 0 || j == sides) && map[i][j] != '1')
+            {
+                return (0);
+            }
+            else if (!check_exit_player(map) || !chek_coins(map))
+                return (0);
+            ++j;
+        }
+        ++i;
+    }
+    return (1);
+}
